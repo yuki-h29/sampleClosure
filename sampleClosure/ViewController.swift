edd318e2b84ba4bef47083f669ca19295e3490ee
yuki-h29 <yuki.hirano29@gmail.com>
2023-07-24 23:01:09 +0900

    Initial Commit

---
+++ sampleClosure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sampleClosure
+//
+//  Created by 平野裕貴 on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sampleClosureUITests/sampleClosureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sampleClosureUITestsLaunchTests.swift
+//  sampleClosureUITests
+//
+//  Created by 平野裕貴 on 2023/07/24.
+//
+
+import XCTest
+
+final class sampleClosureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
